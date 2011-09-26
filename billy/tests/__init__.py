aa634e8374bf2e976515c6f18f8bcfd9fb1477f9
James Turk <james.p.turk@gmail.com>
2011-09-26 17:49:04 -0400

    teardown test db

--- billy/tests/__init__.py
+++ billy/tests/__init__.py
@@ -8,3 +8,10 @@ def setup():
     db_name = settings.MONGO_DATABASE + '_test'
 
     db._db = pymongo.Connection(host, port)[db_name]
+
+def teardown():
+    host = settings.MONGO_HOST
+    port = settings.MONGO_PORT
+    db_name = settings.MONGO_DATABASE + '_test'
+
+    pymongo.Connection(host, port).drop_database(db_name)
