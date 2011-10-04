55878c7e8edeeafbc09ef5c11d975f018755e379
James Turk <james.p.turk@gmail.com>
2011-10-04 17:45:52 -0400

    fulltext.tasks

---
+++ billy/fulltext/tasks.py
@@ -0,0 +1,17 @@
+from oyster.tasks import ExternalStoreTask
+from superfastmatch import Client
+
+class SuperFastMatchTask(ExternalStoreTask):
+
+    external_store = 'superfastmatch'
+    sfm_client = Client('http://ec2-174-129-105-103.compute-1.amazonaws.com/')
+
+    def upload_document(self, doc_id, filedata, metadata):
+        # from {state}.fulltext import extract_text
+        extract_text = __import__('%s.fulltext' % metadata['state'],
+                                  fromlist=['extract_text']).extract_text
+        _id = int(time.time()*10000)
+        extracted = extract_text(filedata, metadata)
+        self.sfm_client.add(1, id, filedata, defer=True,
+                            **metadata)
+        return _id
