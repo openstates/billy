fb93f95b04bdaaca064a3c9c1c3f6876025872ca
James Turk <james.p.turk@gmail.com>
2011-10-05 15:52:44 -0400

    just rely on extract_text arg of ExternalStoreTask

--- billy/fulltext/tasks.py
+++ billy/fulltext/tasks.py
@@ -1,13 +1,7 @@
-import sys
 import time
 
 from oyster.tasks import ExternalStoreTask
 from superfastmatch import Client
-
-from billy.conf import settings
-
-for p in settings.SCRAPER_PATHS:
-    sys.path.insert(0, p)
 
 class SuperFastMatchTask(ExternalStoreTask):
 
@@ -15,11 +9,7 @@ class SuperFastMatchTask(ExternalStoreTask):
     sfm_client = Client('http://ec2-174-129-105-103.compute-1.amazonaws.com/')
 
     def upload_document(self, doc_id, filedata, metadata):
-        # from {state}.fulltext import extract_text
-        extract_text = __import__('%s.fulltext' % metadata['state'],
-                                  fromlist=['extract_text']).extract_text
         _id = int(time.time()*10000)
-        extracted = extract_text(filedata, metadata)
         self.sfm_client.add(1, _id, extracted, defer=True,
                             **metadata)
         return _id
