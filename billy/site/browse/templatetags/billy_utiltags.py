af84ef92bb2d7408f06a10a215b20f6c1c4341cb
= <=>
2012-01-11 17:05:03 -0500

    added decimal_format template tag

--- billy/site/browse/templatetags/billy_utiltags.py
+++ billy/site/browse/templatetags/billy_utiltags.py
@@ -1,4 +1,8 @@
+from decimal import Decimal, Context, Inexact
+import pdb
+
 from django import template
+
 
 register = template.Library()
 
@@ -6,3 +10,9 @@ register = template.Library()
 def sorted_items(value):
     return sorted(value.items())
 
+@register.filter
+def decimal_format(value, TWOPLACES=Decimal(10) ** -2 ):
+    n = Decimal(str(value))
+    n = n.quantize(TWOPLACES)#, context=Context(traps=[Inexact]))
+    return n
+        
