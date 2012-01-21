1e255d720dda071d73a60edda980cb9bc2838cc7
James Turk <james.p.turk@gmail.com>
2012-01-21 01:06:51 -0500

    robots.txt view

--- billy/site/urls.py
+++ billy/site/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
+    (r'robots.txt', 'django.views.generic.simple.direct_to_template',
+     {'template': 'robots.txt'}),
     (r'^api/', include('billy.site.api.urls')),
     (r'^browse/', include('billy.site.browse.urls')),
 )
