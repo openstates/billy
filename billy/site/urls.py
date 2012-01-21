50dd7db1808d39cf618305813255bd193d89a37f
James Turk <james.p.turk@gmail.com>
2012-01-21 01:14:59 -0500

    robots.txt in nginx instead

--- billy/site/urls.py
+++ billy/site/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'robots.txt', 'django.views.generic.simple.direct_to_template',
-     {'template': 'robots.txt'}),
     (r'^api/', include('billy.site.api.urls')),
     (r'^browse/', include('billy.site.browse.urls')),
 )
