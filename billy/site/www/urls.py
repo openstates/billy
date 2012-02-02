d97dab83378e74fa298b61125649e9aeb78c7738
Thom Neale <twneale@gmail.com>
2012-02-02 16:08:13 -0500

    changed app name

--- billy/site/www/urls.py
+++ billy/site/www/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 
 urlpatterns = patterns('',
-    url(r'^(?P<abbr>[a-z]{2})/$', 'billy.site.web.views.state', name='state'),
+    url(r'^(?P<abbr>[a-z]{2})/$', 'billy.site.www.views.state', name='state'),
 )
 
 
