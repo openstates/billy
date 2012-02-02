175bfee356ee303b3c052eb9395eddce2cd62969
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-02-02 15:38:58 -0500

    Adding in a new test, templates and some bugfixes.

--- billy/tests/importers/test_merge.py
+++ billy/tests/importers/test_merge.py
@@ -16,8 +16,26 @@ def _load_test_data( test_name ):
     
     return ( leg1, leg2, mrgd )
 
+def delt(a, b):
+    return dict([
+        (key, b.get(key, a.get(key)))
+        for key in set(a.keys() + b.keys())
+        if (key in a and (not key in b or set(b[key]) != set(a[key])))
+        or (key in b and (not key in a or set(a[key]) != set(b[key])))
+    ])
+
 def _check_results( one, two ):
-    return set(one) == set(two)
+    delta = delt(one, two)
+    if delta != {}:
+        print ""
+        print "One:", one
+        print ""
+        print "Two:", two
+        print ""
+        print "Delta:", delta
+        print ""
+    return delta == {}
+
 
 def _test_logic( name ):
     leg1, leg2, compare = _load_test_data( name )
@@ -26,8 +44,16 @@ def _test_logic( name ):
 ##########
 # Cut below the line
 
+def test_silly_delt_fn():
+    l1 = { "foo" : [ "1", "2" ] }
+    l2 = { "foo" : [ "2", "1" ] }
+    assert delt( l1, l2 ) == {}
+
 def test_legid_sanity():
     _test_logic( "leg_id_sanity" )
 
 def test_scraped_name_sanity():
     _test_logic( "scraped_name_sanity" )
+
+def test_locked_sanity():
+    _test_logic( "locked_field_sanity" )
