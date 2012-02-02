7eeb3c02ba4502265af087a19b034a3e8b4a5c5b
Thom Neale <twneale@gmail.com>
2012-02-02 15:51:25 -0500

    started web app

---
+++ billy/site/web/viewdata/overview.py
@@ -0,0 +1,44 @@
+import pdb
+
+from collections import defaultdict
+
+from billy import db
+
+def chamber(abbr, chamber):
+	'''
+	2-12-2012
+	Per wireframes/spec, provide this info:
+
+	- leg'r count
+	- party breakdown
+	- committee count
+	- joint committee count
+	- bill count
+	'''
+	res = {}
+
+	# Legislators
+	legislators = db.legislators.find({'state': abbr, 'chamber': chamber}, 
+	                                  {'party': True})
+	legislators = list(legislators)
+
+	# Maybe later, mapreduce instead
+	party_counts = defaultdict(int)
+	for leg in legislators:
+		party_counts[leg['party']] += 1
+
+	res['legislators'] = {
+		'count': len(legislators),
+		'party_counts': dict(party_counts),
+		}
+
+	# Committees
+	res['committees'] = {
+	    'count': db.committees.find({'state': abbr, 'chamber': chamber}).count(),
+	    'joint_count': db.committees.find({'state': abbr, 'chamber': 'joint'}).count(),
+	    }
+
+	res['bills_count'] = db.bills.find({'state': abbr, 'chamber': chamber}).count()
+
+	return res
+
