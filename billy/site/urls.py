7614798f9c855720dcf3db7db9ec55b88211aba9
Thom Neale <twneale@gmail.com>
2012-02-02 15:51:05 -0500

    included /web/ urls

--- billy/site/urls.py
+++ billy/site/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^api/', include('billy.site.api.urls')),
     (r'^browse/', include('billy.site.browse.urls')),
+    (r'^web/', include('billy.site.web.urls')),
 )
