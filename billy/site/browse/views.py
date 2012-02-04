ad4f49df8f1b185f0f1d7891916ecddc513bff30
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-02-03 22:55:50 -0500

    D'oh! DB writes restored.

--- billy/site/browse/views.py
+++ billy/site/browse/views.py
@@ -418,11 +418,11 @@ def mom_commit(request):
     merged, remove = merge_legislators( leg1, leg2 )
     actions.append( "Merged Legislators as '%s'" % merged['leg_id'] )
 
-    #db.legislators.remove({ '_id' : remove }, safe=True)
+    db.legislators.remove({ '_id' : remove }, safe=True)
     actions.append( "Deleted Legislator (which had the ID of %s)" %
         remove )
 
-    #db.legislators.save( merged, safe=True )
+    db.legislators.save( merged, safe=True )
     actions.append( "Saved Legislator %s with merged data" % merged['leg_id'] )
 
     for attr in merged:
