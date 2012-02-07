openstates/billy

commit 25519f079219dc4a79f234892cdaa030b594e158

Pushing up the base for executing the change

billy/site/browse/views.py
@@ -14,7 +14,7 @@ import pymongo
 
 from django.http import Http404, HttpResponse
 from django.views.decorators.cache import never_cache
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template.loader import render_to_string
 
 from billy import db
@@ -37,7 +37,7 @@ def _csv_response(request, template, data):
             out.writerow(item)
         return resp
     else:
-        return render_to_response(template, {'data':data})
+        return render(request, template, {'data':data})
 
 
 
@@ -60,7 +60,7 @@ def browse_index(request, template='billy/index.html'):
 
     rows.sort(key=lambda x: x['name'])
 
-    return render_to_response(template, {'rows': rows})
+    return render(request, template, {'rows': rows})
 
 
 def overview(request, abbr):
@@ -73,7 +73,7 @@ def overview(request, abbr):
     context['metadata'] = meta
     context['report'] = report
 
-    return render_to_response('billy/state_index.html', context)
+    return render(request, 'billy/state_index.html', context)
 
 
 @never_cache
@@ -138,7 +138,7 @@ def bills(request, abbr):
     render = functools.partial(render_to_string, 'billy/bills_table.html')
     tables = map(render, tables)
 
-    return render_to_response("billy/bills.html",
+    return render(request, "billy/bills.html",
                               dict(tables=tables, metadata=meta,
                                    sessions=sessions))
 
@@ -168,7 +168,7 @@ def summary_index(request, abbr):
         return res
     
     summary = build_state(abbr)
-    return render_to_response('billy/summary_index.html', locals())
+    return render(request, 'billy/summary_index.html', locals())
 
 
 def summary_object_key(request, abbr, urlencode=urllib.urlencode,
@@ -212,7 +212,7 @@ def summary_object_key(request, abbr, urlencode=urllib.urlencode,
     objs = sorted(counter, key=counter.get, reverse=True)
     objs = ((obj, counter[obj], counter[obj]/total, params(obj)) for obj in objs)
     
-    return render_to_response('billy/summary_object_key.html', locals())
+    return rendert(request, 'billy/summary_object_key.html', locals())
 
 
     
@@ -242,7 +242,7 @@ def summary_object_key_vals(request, abbr, urlencode=urllib.urlencode,
 
     spec = json.dumps(spec, cls=JSONDateEncoder, indent=4)
 
-    return render_to_response('billy/summary_object_keyvals.html', locals())
+    return render(request, 'billy/summary_object_keyvals.html', locals())
 
     
 def object_json(request, collection, _id,
@@ -271,7 +271,7 @@ def object_json(request, collection, _id,
     obj_json = re.sub('"(http://.+?)"',
                       lambda m: tmpl.format(*m.groups()), obj_json)
     
-    return render_to_response('billy/object_json.html', locals())
+    return render(request, 'billy/object_json.html', locals())
     
 
 def other_actions(request, abbr):
@@ -321,7 +321,7 @@ def random_bill(request, abbr):
     count = db.bills.find(spec).count()
     bill = db.bills.find(spec)[random.randint(0, count - 1)]
 
-    return render_to_response('billy/bill.html', {'bill': bill})
+    return render(request, 'billy/bill.html', {'bill': bill})
 
 
 def bill(request, abbr, session, id):
@@ -331,7 +331,7 @@ def bill(request, abbr, session, id):
     if not bill:
         raise Http404
 
-    return render_to_response('billy/bill.html', {'bill': bill})
+    return render(request, 'billy/bill.html', {'bill': bill})
 
 
 def bill_json(request, abbr, session, id):
@@ -343,7 +343,7 @@ def bill_json(request, abbr, session, id):
 
     _json = json.dumps(bill, cls=JSONDateEncoder, indent=4)
 
-    return render_to_response('billy/bill_json.html', {'json': _json})
+    return render(request, 'billy/bill_json.html', {'json': _json})
 
 
 def legislators(request, abbr):
@@ -360,7 +360,7 @@ def legislators(request, abbr):
     lower_legs = sorted(lower_legs, key=keyfunc)
     inactive_legs = sorted(inactive_legs, key=lambda x: x['last_name'])
 
-    return render_to_response('billy/legislators.html', {
+    return response(request, 'billy/legislators.html', {
         'upper_legs': upper_legs,
         'lower_legs': lower_legs,
         'inactive_legs': inactive_legs,
@@ -375,7 +375,7 @@ def legislator(request, id):
 
     meta = metadata(leg[leg['level']])
 
-    return render_to_response('billy/legislator.html', {'leg': leg,
+    return render(request, 'billy/legislator.html', {'leg': leg,
                                                         'metadata': meta})
 
 
@@ -393,7 +393,7 @@ def committees(request, abbr):
     lower_coms = sorted(lower_coms)
     joint_coms = sorted(joint_coms)
 
-    return render_to_response('billy/committees.html', {
+    return render( request, 'billy/committees.html', {
         'upper_coms': upper_coms,
         'lower_coms': lower_coms,
         'joint_coms': joint_coms,
@@ -401,7 +401,20 @@ def committees(request, abbr):
     })
 
 def mom_index(request):
-    return render_to_response('billy/mom_index.html')
+    return render(request, 'billy/mom_index.html' )
+
+def mom_commit(request):
+    leg1 = request.POST['leg1']
+    leg2 = request.POST['leg2']
+
+    leg1 = db.legislators.find_one({'_id' : leg1 })
+    leg2 = db.legislators.find_one({'_id' : leg2 })
+
+    merged = merge_legislators( leg1, leg2 )
+
+    return render( request, 'billy/mom_commit.html', {
+        "merged" : merged
+        })
 
 def mom_merge(request):
     leg1 = "leg1"
@@ -436,7 +449,8 @@ def mom_merge(request):
         else:
             mv[key] = "N"
 
-    return render_to_response('billy/mom_merge.html', {
+    return render(request, 'billy/mom_merge.html', {
        'leg1'  : leg1, 'leg2' : leg2,
        'merge' : merge, 'merge_view' : mv,
        'merge_view_info' : mv_info })
+

billy/site/browse/templates/billy/mom_commit.html
@@ -1,0 +1,21 @@
+{% extends "billy/base.html" %}
+{% load humanize %}
+{% load billy_utiltags %}
+
+{% block content %}
+    <div class = 'container' >
+        <div class="hero-unit">
+            <h1>Merge Complete <small>Ta-da.</small></h1>
+            <p>The merge <b>has</b> been executed. If there was a problem with
+            this merge, pleas <i>please</i> get it straightened out ASAP. Causing
+            disruption to the API users is pretty much the worst-case, so let's
+            make sure we maintain sanity.
+            <br />
+            <br />
+            The merge is shown below just for last-stage verification of the
+            results. Please do pay attention to it. Feel free to head back
+            home <a href = '{% url mom_index %}' >here</a>
+            </p>
+        </div>
+    </div>
+{% endblock %}

billy/site/browse/templates/billy/mom_merge.html
@@ -12,6 +12,7 @@
     <script type = 'text/javascript' >
 
 $(document).ready(function() {
+    /* Toggle the unmerging data */
     var _toggle_status = true; // hack!
     $(".root-merge-U").hide();
     $("#show-hidden").click(function() {
@@ -69,5 +70,21 @@ $(document).ready(function() {
                 {% endfor %}
             <table>
         </div>
+        <h1>Approve this change</h1>
+        <p>
+            Approving this change means that
+            you've looked over this data (very carefuly) and you consider this to
+            be a valid and meaningful merge that will result in <b>no</b> net
+            data loss. If you're not at all sure, this is your last chance to
+            avoid the merge.
+        </p>
+        <form action = '{% url mom_commit %}' method = 'POST' >
+            <input name = 'leg1' type = 'hidden' value = '{{ leg1.leg_id }}' />
+            <input name = 'leg2' type = 'hidden' value = '{{ leg2.leg_id }}' />
+            {% csrf_token %}
+            &nbsp;
+            <input name = 'sub'  type = 'submit' value = 'Make it so!'
+                class = 'btn danger' />
+        </form>
     </div>
 {% endblock %}

billy/site/browse/urls.py
@@ -23,6 +23,7 @@ urlpatterns = patterns('billy.site.browse.views',
     url(r'^legislators/(?P<id>.*)/$', 'legislator'),
 
     # Merge-o-matic URLs.
-    url(r'^mom$', 'mom_index'),
+    url(r'^mom$', 'mom_index', name="mom_index" ),
     url(r'^mom/merge$', 'mom_merge', name="mom_merge" ),
+    url(r'^mom/commit$', 'mom_commit', name="mom_commit" )
 )

commit 11622ae3d7845bbbd53a5ec21018b14ae583cd93

fixing the issues introduced in 25519f07.
 Sorry for messing it up, @twneale

billy/site/browse/views.py
@@ -15,7 +15,7 @@ import pymongo
 
 from django.http import Http404, HttpResponse
 from django.views.decorators.cache import never_cache
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.template.loader import render_to_string
 
 from billy import db
@@ -139,7 +139,7 @@ def bills(request, abbr):
     render = functools.partial(render_to_string, 'billy/bills_table.html')
     tables = map(render, tables)
 
-    return render(request, "billy/bills.html",
+    return render_to_response( "billy/bills.html",
                               dict(tables=tables, metadata=meta,
                                    sessions=sessions))
 