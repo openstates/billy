9004f44dc121e2623f6fc5dfee60ff6b0147ab38
James Turk <james.p.turk@gmail.com>
2012-02-23 17:42:30 -0500

    retab models test

--- billy/tests/models/tests.py
+++ billy/tests/models/tests.py
@@ -7,119 +7,119 @@ from billy.models import models_list
 from billy import db
 
 class TestNoConflict(unittest.TestCase):
-	'''
-	Make sure no model attribute names conflict with document keys.
-	'''
-	def conflict_exists(self, model_attrs, doc_keys):
-		return model_attrs & doc_keys
+    '''
+    Make sure no model attribute names conflict with document keys.
+    '''
+    def conflict_exists(self, model_attrs, doc_keys):
+        return model_attrs & doc_keys
 
-	def test_all_models(self):
-		conflict_exists = self.conflict_exists
-		for model in models_list:
-			model_attrs = set(dir(model))
-			for document in model.collection.find():
-				doc_keys = set(document)
-				self.assertFalse(conflict_exists(model_attrs, doc_keys))
+    def test_all_models(self):
+        conflict_exists = self.conflict_exists
+        for model in models_list:
+            model_attrs = set(dir(model))
+            for document in model.collection.find():
+                doc_keys = set(document)
+                self.assertFalse(conflict_exists(model_attrs, doc_keys))
 
-	def test_bogus_key(self, model=Bill):
-		document = model.collection.find_one()
-		model_attrs = set(dir(model))
-		doc_keys = set(document)
-		bogus_key = choice(list(model_attrs - doc_keys))
-		document[bogus_key] = 3
-		doc_keys = set(document)
-		self.assertTrue(self.conflict_exists(model_attrs, doc_keys))
+    def test_bogus_key(self, model=Bill):
+        document = model.collection.find_one()
+        model_attrs = set(dir(model))
+        doc_keys = set(document)
+        bogus_key = choice(list(model_attrs - doc_keys))
+        document[bogus_key] = 3
+        doc_keys = set(document)
+        self.assertTrue(self.conflict_exists(model_attrs, doc_keys))
 
 
 class TestDereferencing(unittest.TestCase):
 
-	def test_state_legislators(self):
+    def test_state_legislators(self):
 
-		# Pick a state.
-		meta = list(db.metadata.find())
+        # Pick a state.
+        meta = list(db.metadata.find())
 
-		for m in meta:
+        for m in meta:
 
-			# First get the state's legislator's manually.
-			legislators1 = list(db.legislators.find({'state': m['_id']}))
+            # First get the state's legislator's manually.
+            legislators1 = list(db.legislators.find({'state': m['_id']}))
 
-			# Now get them the spiffy way.
-			legislators2 = list(Metadata(m).legislators())
+            # Now get them the spiffy way.
+            legislators2 = list(Metadata(m).legislators())
 
-			self.assertEqual(legislators1, legislators2)
-
-
-	def test_state_committees(self):
-
-		# Pick a state.
-		meta = list(db.metadata.find())
-
-		for m in meta:
-
-			# First get the state's legislator's manually.
-			committees1 = list(db.committees.find({'state': m['_id']}))
-
-			# Now get them the spiffy way.
-			committees2 = list(Metadata(m).committees())
-
-			self.assertEqual(committees1, committees2)
+            self.assertEqual(legislators1, legislators2)
 
 
-	def test_legislator_committees(self):
+    def test_state_committees(self):
 
-		# Pick a state.
-		for leg in db.legislators.find():
-				
-			leg_id = leg['_id']
+        # Pick a state.
+        meta = list(db.metadata.find())
 
-			# Get the committees manually. 
-			committees1 = list(db.committees.find({'members.leg_id': leg_id}))
+        for m in meta:
 
-			# Now the spiffy way.
-			committees2 = list(Legislator(leg).committees())
+            # First get the state's legislator's manually.
+            committees1 = list(db.committees.find({'state': m['_id']}))
 
-			self.assertEqual(committees1, committees2)
+            # Now get them the spiffy way.
+            committees2 = list(Metadata(m).committees())
+
+            self.assertEqual(committees1, committees2)
 
 
-	def test_legislator_sponsored_bills(self):
+    def test_legislator_committees(self):
 
-		# Pick a state.
-		for leg in db.legislators.find():
-				
-			leg_id = leg['_id']
+        # Pick a state.
+        for leg in db.legislators.find():
+                
+            leg_id = leg['_id']
 
-			# Get the legislator's bills manually. 
-			bills1 = list(db.bills.find({'sponsors.leg_id': leg_id}))
+            # Get the committees manually. 
+            committees1 = list(db.committees.find({'members.leg_id': leg_id}))
 
-			# Now the spiffy way.
-			bills2 = list(Legislator(leg).sponsored_bills())
+            # Now the spiffy way.
+            committees2 = list(Legislator(leg).committees())
 
-			self.assertEqual(bills1, bills2)
+            self.assertEqual(committees1, committees2)
 
-	def test_committeemember_legislator(self):
 
-		for c in db.committees.find():
+    def test_legislator_sponsored_bills(self):
 
-			for member in c['members']:
+        # Pick a state.
+        for leg in db.legislators.find():
+                
+            leg_id = leg['_id']
 
-				leg_id = member['leg_id']
+            # Get the legislator's bills manually. 
+            bills1 = list(db.bills.find({'sponsors.leg_id': leg_id}))
 
-				if leg_id is None:
-					continue
+            # Now the spiffy way.
+            bills2 = list(Legislator(leg).sponsored_bills())
 
-				# Get the legislator manually...
-				leg1 = db.legislators.find_one(leg_id)
+            self.assertEqual(bills1, bills2)
 
-				# And the spiffy way.
-				leg2 = CommitteeMember(member).legislator_object()
+    def test_committeemember_legislator(self):
 
-				if (leg1 is None) or (leg2 is None):
-					pdb.set_trace()
+        for c in db.committees.find():
 
-				self.assertEqual(leg1, leg2)
+            for member in c['members']:
+
+                leg_id = member['leg_id']
+
+                if leg_id is None:
+                    continue
+
+                # Get the legislator manually...
+                leg1 = db.legislators.find_one(leg_id)
+
+                # And the spiffy way.
+                leg2 = CommitteeMember(member).legislator_object()
+
+                if (leg1 is None) or (leg2 is None):
+                    pdb.set_trace()
+
+                self.assertEqual(leg1, leg2)
 
 
 if __name__ == "__main__":
-	unittest.main()				
+    unittest.main()             
 
 
