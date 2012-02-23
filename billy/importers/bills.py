ed256db610163f796379c32beea1a3db656ae034
James Turk <james.p.turk@gmail.com>
2012-02-23 17:12:08 -0500

    fix case of missing alternate_bill_ids

--- billy/importers/bills.py
+++ billy/importers/bills.py
@@ -85,7 +85,7 @@ def import_bill(data, votes, categorizer):
     # clean up bill_ids
     data['bill_id'] = fix_bill_id(data['bill_id'])
     data['alternate_bill_ids'] = [fix_bill_id(bid) for bid in
-                                  data['alternate_bill_ids']]
+                                  data.get('alternate_bill_ids', [])]
 
     # move subjects to scraped_subjects
     # NOTE: intentionally doesn't copy blank lists of subjects
