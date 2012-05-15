bb38d52b9e294ad56d47baaedb23c45661b05467
Thom Neale <twneale@gmail.com>
2012-05-15 09:10:18 -0400

    public: added views for 'more votes', 'more news'

--- billy/web/public/views.py
+++ billy/web/public/views.py
@@ -1,34 +1,40 @@
 import re
-import pdb
+import operator
 from functools import wraps
 from itertools import repeat, islice
 
 import pymongo
 
-from django.views.generic.base import TemplateView
 from django.shortcuts import render, redirect, render_to_response
-from django.template import RequestContext, loader
-from django.core.urlresolvers import reverse
+from django.template import RequestContext
+from django.views.generic import TemplateView
 
-from billy.models import db, Bill, Metadata, Legislator, Committee
+import billy.models
+from billy.models import db, Metadata
+from billy.models.pagination import CursorPaginator, IteratorPaginator
 
-from .context_processors import default_processor
 from .forms import StateSelectForm
 from .viewdata import overview
 
 
 default_context = dict(base_template='billy/web/public/base.html')
 
+
 def nth(iterable, n, default=None):
     "Returns the nth item or a default value"
     return next(islice(iterable, n, None), default)
 
+
 repeat1 = repeat(1)
+
+
 def include_fields(*field_names):
     return zip(field_names, repeat1)
 
+
 def templatename(name):
     return 'billy/web/public/%s.html' % name
+
 
 def simplified(f):
     '''Render the decorated view to response with the template
@@ -42,6 +48,78 @@ def simplified(f):
         return render(request, template, dictionary)
 
     return wrapper
+
+
+class ListViewBase(TemplateView):
+    'Base class for VoteList, FeedList, etc.'
+    template_name = templatename('object_list')
+
+    def get_context_data(self, *args, **kwargs):
+        super(ListViewBase, self).get_context_data(*args, **kwargs)
+        context = {}
+        context.update(**default_context)
+        context.update(column_headers=self.column_headers,
+                       rowtemplate_name=self.rowtemplate_name,
+                       object_list=self.get_queryset(),
+                       statenav_active=self.statenav_active,
+                       abbr=self.kwargs['abbr'],
+                       url=self.request.path,
+                       use_table=getattr(self, 'use_table', False))
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        collection_name = self.kwargs.get('collection_name')
+        collection_name = {
+            'state': 'metadata',
+            }.get(collection_name, collection_name)
+        _id = self.kwargs.get('id')
+
+        get = self.request.GET.get
+        show_per_page = int(get('show_per_page', 20))
+        page = int(get('page', 1))
+        if 100 < show_per_page:
+            show_per_page = 100
+        collection = getattr(billy.models.db, collection_name)
+        obj = collection.find_one(_id)
+        objects = getattr(obj, self.query_attr)
+        if callable(objects):
+            objects = objects()
+        sort_func = getattr(self, 'sort_func', None)
+        sort_reversed = bool(getattr(self, 'sort_reversed', None))
+        if sort_func:
+            objects = sorted(objects, key=sort_func,
+                             reverse=sort_reversed)
+        paginator = self.paginator(objects, page=page,
+                                   show_per_page=show_per_page)
+        return paginator
+
+
+class VotesList(ListViewBase):
+
+    list_item_context_name = 'vote'
+    sort_func = operator.itemgetter('date')
+    sort_reversed = True
+    paginator = IteratorPaginator
+    query_attr = 'votes_manager'
+    use_table = True
+
+    rowtemplate_name = templatename('votes_list_row')
+    column_headers = ('Bill', 'Date', 'Outcome', 'Yes',
+                      'No', 'Other', 'Motion')
+    statenav_active = 'bills'
+
+
+class FeedsList(ListViewBase):
+
+    list_item_context_name = 'entry'
+    paginator = CursorPaginator
+    query_attr = 'feed_entries'
+
+    rowtemplate_name = templatename('feed_entry')
+    column_headers = ('feeds',)  # ('date', 'title', 'author', 'host',
+                      #'summary', 'tags')
+    statenav_active = 'bills'
+
 
 def state_nav(active_collection):
     'Produce data for the state navigation bar.'
@@ -70,6 +148,7 @@ def state(request, abbr):
             recent_actions=overview.recent_actions(abbr),
             statenav_active=None),
         context_instance=RequestContext(request, default_context))
+
 
 def state_selection(request):
     '''
@@ -141,7 +220,6 @@ def legislator(request, abbr, leg_id):
     Note - changes needed before we can display "sessions served" info.
     '''
     legislator = db.legislators.find_one({'_id': leg_id})
-    roles = list(legislator.roles_manager)
 
     # Note to self: Slow query
     sponsored_bills = legislator.sponsored_bills(
@@ -154,6 +232,7 @@ def legislator(request, abbr, leg_id):
         template_name=templatename('legislator'),
         dictionary=dict(
             feed_entry_template=templatename('feed_entry'),
+            vote_preview_row_template=templatename('vote_preview_row'),
             roles=legislator.roles_manager,
             abbr=abbr,
             metadata=Metadata.get_object(abbr),
@@ -175,7 +254,6 @@ def committees(request, abbr):
 def committees_chamber(request, abbr, chamber):
 
     state = Metadata.get_object(abbr)
-    chamber_name = state['%s_chamber_name' % chamber]
 
     # Query params
     spec = {'chamber': chamber}
@@ -190,7 +268,8 @@ def committees_chamber(request, abbr, chamber):
         sort_key = request.GET['key']
         sort_order = int(request.GET['order'])
 
-    committees = state.committees(spec, fields=fields, sort=[(sort_key, sort_order)])
+    committees = state.committees(spec, fields=fields,
+                                  sort=[(sort_key, sort_order)])
 
     sort_order = {1: -1, -1: 1}[sort_order]
 
@@ -203,20 +282,26 @@ def committee(request, abbr, committee_id):
     sources = committee['sources']
     return locals()
 
-#----------------------------------------------------------------------------
+
 @simplified
 def bills(request, abbr):
     state = Metadata.get_object(abbr)
     return locals()
 
-@simplified
+
 def bill(request, abbr, bill_id):
-    state = Metadata.get_object(abbr)
     bill = db.bills.find_one({'_id': bill_id})
-    sponsors = list(bill.sponsors_manager)
-    first_five = bill.sponsors_manager.first_five()
-    sources = bill['sources']
-    return locals()
+    return render_to_response(
+        template_name=templatename('bill'),
+        dictionary=dict(
+            vote_preview_row_template=templatename('vote_preview_row'),
+            abbr=abbr,
+            state=Metadata.get_object(abbr),
+            bill=bill,
+            first_five=bill.sponsors_manager.first_five(),
+            sources=bill['sources'],
+            statenav_active='bills'),
+        context_instance=RequestContext(request, default_context))
 
 
 #----------------------------------------------------------------------------
