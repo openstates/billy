62b02a1440a0e9712d9cf4171666cf1a675fd54b
Thom Neale <twneale@gmail.com>
2012-05-22 16:05:15 -0400

    public: middleware to limit public views to release states; fixes # 11

---
+++ billy/web/public/middleware.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+
+from billy.web.public.views import state_not_active_yet
+
+
+class LimitStatesMiddleware(object):
+    '''This middle ware checks state-specific requests to
+    ensure the state is in settings.ACTIVE_STATES. If not,
+    it routes the user to a page that explains the state isn't
+    available yet.
+    '''
+    def process_view(self, request, func, args, kw):
+
+        # Skip API or admin views.
+        for path in ['/admin', '/api']:
+            if request.path.startswith(path):
+                return func(request, *args, **kw)
+
+        # For public views, make sure the state is active.
+        if 'abbr' in kw:
+            if kw['abbr'] not in settings.ACTIVE_STATES:
+                return state_not_active_yet(request, args, kw)
+            else:
+                return func(request, *args, **kw)
