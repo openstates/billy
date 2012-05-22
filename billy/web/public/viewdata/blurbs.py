7ece964d5c71f25f822c6745d05d1ae281f765e9
Thom Neale <twneale@gmail.com>
2012-05-22 16:06:03 -0400

    public: misc cleanup

--- billy/web/public/viewdata/blurbs.py
+++ billy/web/public/viewdata/blurbs.py
@@ -2,6 +2,5 @@ from django.template.loader import render_to_string
 
 
 def bio_blurb(legislator):
-    #import pdb;pdb.set_trace()
     return render_to_string('billy/web/public/bio_blurb.html',
                             dict(legislator=legislator))
