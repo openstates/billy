66092728868b5cb5e6322204864f0a0e318fd2a1
Thom Neale <twneale@gmail.com>
2012-05-22 14:14:13 -0400

    models: tweak Legislastors.primary_sponsored_bills

--- billy/web/public/viewdata/overview.py
+++ billy/web/public/viewdata/overview.py
@@ -1,15 +1,14 @@
-import pdb
-
+import re
 from collections import defaultdict
 from itertools import repeat
 from operator import itemgetter
 
-import pymongo
-
-from billy.models import db, Bill, Metadata, Legislator, Committee
+from billy.models import Metadata
 
 
 repeat1 = repeat(1)
+
+
 def include_fields(*field_names):
     return zip(field_names, repeat1)
 
@@ -27,10 +26,11 @@ def chamber(abbr, chamber):
     '''
     res = {}
 
-    state = Metadata.get_object(abbr)   
+    state = Metadata.get_object(abbr)
 
     # Legislators
-    legislators = state.legislators({'chamber': chamber}, {'party': True})
+    legislators = state.legislators({'chamber': chamber, 'active': True},
+                                    {'party': True})
     legislators = list(legislators)
 
     # Maybe later, mapreduce instead
@@ -53,23 +53,28 @@ def chamber(abbr, chamber):
 
     return res
 
+
 def recent_actions(abbr):
-    state = Metadata.get_object(abbr)   
+    state = Metadata.get_object(abbr)
     bills = state.bills({'session': state.most_recent_session,
-                         'actions.type': 'bill:passed',
-                         'actions.type': 'bill:introduced', 
+                         '$or': [{'actions.type': 'bill:passed',
+                                  'actions.type': 'bill:introduced'}],
                          'type': 'bill'})
     bills_by_action = defaultdict(list)
     for bill in bills:
         for action in bill['actions']:
-            actor = action['actor']
+            actor = re.search(r'(upper|lower)', action['actor'])
+            if actor:
+                actor = actor.group()
+            else:
+                continue
             for type_ in action['type']:
                 if type_ in ['bill:passed', 'bill:introduced']:
                     bills_by_action[(type_, actor)].append(
                                             (action['date'], bill))
 
-    def f(type_, chamber): 
-        bills = list(sorted(bills_by_action[(type_, chamber)], 
+    def f(type_, chamber):
+        bills = list(sorted(bills_by_action[(type_, chamber)],
                      reverse=True, key=itemgetter(0)))[:2]
         return map(itemgetter(1), bills)
 
@@ -79,5 +84,6 @@ def recent_actions(abbr):
         introduced_upper=f('bill:introduced', 'upper'),
         introduced_lower=f('bill:introduced', 'lower'))
 
-    return res
+    import pdb;pdb.set_trace()
 
+    return res
