0b9c0877c2b5c9787436b0d9a2f881a7ffb91026
Thom Neale <twneale@gmail.com>
2012-05-29 16:37:53 -0400

    public: temporarily throw error for inactive leg bio blurbs

--- billy/web/public/viewdata/blurbs.py
+++ billy/web/public/viewdata/blurbs.py
@@ -6,6 +6,7 @@ def bio_blurb(legislator):
         return render_to_string('billy/web/public/bio_blurb.html',
                                 dict(legislator=legislator))
     else:
+        raise NotImplementedError('Bio blurbs for inactive legislators don\t work yet.')
         for role in legislator.old_roles_manager():
             import pdb;pdb.set_trace()
         return render_to_string('billy/web/public/bio_blurb_inactive.html',
