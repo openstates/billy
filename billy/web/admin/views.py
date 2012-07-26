a56e1b399c316307e3cce9032b510a287bcf1790
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-07-26 13:45:29 -0400

    Adding in muti-subjects.

--- billy/web/admin/views.py
+++ billy/web/admin/views.py
@@ -875,15 +875,17 @@ def subjects_commit(request, abbr):
 
     for idex in payload:
         key, val = idex.split("-", 1)
+        if val == 'remote':
+            catd_subjects[key]['normal'] = []
         catd_subjects[key][val] = payload[idex]
 
     for idex in catd_subjects:
         sub = catd_subjects[idex]
 
         remote = sub['remote'][0].strip()
-        normal = sub['normal'][0].strip()
+        normal = [x.strip() for x in sub['normal']]
 
-        if normal == "":
+        if normal == []:
             continue
 
         eyedee = _gen_id(abbr, remote)
