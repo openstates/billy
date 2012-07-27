293b21b0deba5e18294b79b983816f39df6e3d82
James Turk <james.p.turk@gmail.com>
2012-07-27 14:33:24 -0400

    subjects fix

--- billy/web/public/views/bills.py
+++ billy/web/public/views/bills.py
@@ -128,7 +128,7 @@ class RelatedBillsList(RelatedObjectsList):
 
             subjects = form.data.get('subjects')
             if subjects:
-                spec['subjects'] = {'$all': filter(None, subjects)}
+                spec['subjects'] = {'$all': [filter(None, subjects)]}
 
             sponsor_id = form.data.get('sponsor__leg_id')
             if sponsor_id:
