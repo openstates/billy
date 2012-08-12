3f4010d202df212bfe03d3d2b5606838ca39a1be
Thom Neale <twneale@gmail.com>
2012-08-12 13:04:37 -0400

    stop the p;lague of keyerror emails

--- billy/models/legislators.py
+++ billy/models/legislators.py
@@ -190,7 +190,7 @@ class Legislator(Document):
         chamber = vote['chamber']
 
         # ...and use the bill's chamber too.
-        roles = filter(lambda role: role['chamber'] == chamber, roles)
+        roles = [role for role in roles if role.get('chamber') == chamber]
 
         # ...and the specific date defined by the date of the vote.
         if len(roles) == 1:
