ff7d4fd4ce1b68e2c7d9dbbe7c723a9c6c612ad3
James Turk <james.p.turk@gmail.com>
2012-08-20 18:04:38 -0400

    fix session/term confusion in the new leg_id categorizer

--- billy/importers/names.py
+++ billy/importers/names.py
@@ -242,10 +242,7 @@ class MongoNameMatcher(CSVNameMatcher):
 
         for row in rows:
             (term, chamber, name, leg_id) = (
-                    row['session'],
-                    row['chamber'],
-                    row['name'],
-                    row['leg_id'])
+                    row['term'], row['chamber'], row['name'], row['leg_id'])
 
             if term == self._term and leg_id:
                 self._manual[chamber][name] = leg_id
