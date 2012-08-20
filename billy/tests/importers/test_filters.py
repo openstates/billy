baecd7a0e0e8ee1cb1a781f32a4a1fa9987cc15a
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-08-20 15:57:09 -0400

    Adding in a country code checker

--- billy/tests/importers/test_filters.py
+++ billy/tests/importers/test_filters.py
@@ -13,3 +13,13 @@ def test_phone_filter():
     ]
     for num in numbers:
         assert phone_filter(num) == number
+
+
+def test_phone_filter_country():
+    number = "1-555-606-0842"
+    numbers = [
+        "+1-(555)-606-0842",
+        "+1 (555) 606-0842"
+    ]
+    for num in numbers:
+        assert phone_filter(num) == number
