534d29d47f6304e25de08f5b40c4dca4a89d969e
James Turk <james.p.turk@gmail.com>
2012-08-29 18:41:35 -0400

    use ie feature flag to key off count in admin

--- billy/web/admin/views.py
+++ billy/web/admin/views.py
@@ -82,6 +82,8 @@ def browse_index(request, template='billy/index.html'):
         meta = db.metadata.find_one({'_id': report['_id']})
         report['name'] = meta['name']
         report['unicameral'] = ('lower_chamber_name' not in meta)
+        report['influence_explorer'] = ('influenceexplorer' in
+                                        meta['feature_flags'])
         report['bills']['typed_actions'] = (100 -
             report['bills']['actions_per_type'].get('other', 100))
         rows.append(report)
