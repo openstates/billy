5c75dfef937c0cf4d8d724fd7e8d10dd6a56bcf5
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-11-09 16:02:51 -0500

    cleaning up speach importer a bit

--- billy/importers/speeches.py
+++ billy/importers/speeches.py
@@ -16,31 +16,30 @@ logger = logging.getLogger('billy')
 def import_speeches(abbr, data_dir):
     data_dir = os.path.join(data_dir, abbr)
     pattern = os.path.join(data_dir, 'speeches', '*.json')
-    speech_record_ids = {}
+    speech_record_ids = defaultdict(set)
 
     for path in glob.iglob(pattern):
         # OK, We need to first go through all the JSON and load the document
         # IDs to clear out.
         with open(path) as f:
             data = prepare_obj(json.load(f))
+
         session = data['session']
         chamber = data['chamber']
-        if not session in speech_record_ids:
-            speech_record_ids[session] = defaultdict(set)
 
-        speech_record_ids[session][chamber].add(data['record_id'])
+        speech_record_ids[session].add((chamber, data['record_id']))
 
     for session in speech_record_ids:
-        for chamber in speech_record_ids[session]:
-            for record in speech_record_ids[session][chamber]:
-                # XXX: Should we really be clearing them all up front? Should
-                #      we clear as we process each record block? Is it OK to
-                #      store everything in memory? (there's a lot)
-                #
-                #      this will result in broken data if the import breaks
-                #      below.
-                #  -- PRT
-                clear_old_speeches(abbr, chamber, session, record)
+        for obj in speech_record_ids[session]:
+            chamber, record = obj
+            # XXX: Should we really be clearing them all up front? Should
+            #      we clear as we process each record block? Is it OK to
+            #      store everything in memory? (there's a lot)
+            #
+            #      this will result in broken data if the import breaks
+            #      below.
+            #  -- PRT
+            clear_old_speeches(abbr, chamber, session, record)
 
     for path in glob.iglob(pattern):
         # OK, now we need to import all the JSON. We don't keep the objects
