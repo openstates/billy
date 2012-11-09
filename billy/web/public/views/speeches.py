5946077ed4060e9baea3ddeb1aa67369205c4256
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-11-09 18:23:08 -0500

    Updating the speech stuff a bit

--- billy/web/public/views/speeches.py
+++ billy/web/public/views/speeches.py
@@ -6,23 +6,37 @@ from django.http import Http404, HttpResponse
 from django.contrib.sites.models import Site
 
 import billy
+from billy.core import settings
 from billy.models import db, Metadata
 
 from .utils import templatename
 
 
-def speeches(request, abbr, event_id):
+def speeches_by_event(request, abbr, event_id):
     event = db.events.find_one({
-        '_id': event_id
+        '_id': event_id,
+        settings.LEVEL_FIELD: abbr
     })
 
     speeches = db.speeches.find({
-        "event_id": event_id
+        "event_id": event_id,
+        settings.LEVEL_FIELD: abbr
     }).sort("sequence", 1)
+
+    return render(request, templatename('speeches_by_event'),
+                  dict(abbr=abbr,
+                       metadata=Metadata.get_object(abbr),
+                       speeches=speeches,
+                       event=event))
+
+
+def speeches(request, abbr):
+    events = db.events.find({
+        settings.LEVEL_FIELD: abbr,
+    })
 
     return render(request, templatename('speeches'),
                   dict(abbr=abbr,
                        metadata=Metadata.get_object(abbr),
                        speeches=speeches,
-                       event=event,
-                       nav_active='events'))
+                       events=events))
