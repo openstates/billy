1a0ccea2ca42f0a172c7e3a97fd8e3c56dca433d
James McKinney <james@slashpoundbang.com>
2012-11-29 12:06:32 -0500

    Relax abbreviation regular expression on more URLs

--- billy/web/public/urls.py
+++ billy/web/public/urls.py
@@ -16,15 +16,15 @@ urlpatterns = patterns('billy.web.public.views.misc',
         name='get_district'),
 
     # votes & news
-    url(r'^(?P<abbr>[a-z]{2})/(?P<collection_name>\w+)/(?P<_id>\w+)/'
+    url(r'^(?P<abbr>[a-z-]+)/(?P<collection_name>\w+)/(?P<_id>\w+)/'
         '(?P<slug>[^/]+)/news/$',
         NewsList.as_view(), name='news_list'),
-    url(r'^(?P<abbr>[a-z]{2})/(?P<collection_name>\w+)/(?P<_id>\w+)/'
+    url(r'^(?P<abbr>[a-z-]+)/(?P<collection_name>\w+)/(?P<_id>\w+)/'
         '(?P<slug>[^/]+)/news/rss/$',
         NewsListFeed(), name='news_list_rss'),
-    url(r'^(?P<abbr>[a-z]{2})/(?P<collection_name>\w+)/(?P<_id>\w+)/votes/$',
+    url(r'^(?P<abbr>[a-z-]+)/(?P<collection_name>\w+)/(?P<_id>\w+)/votes/$',
         VotesList.as_view(), name='votes_list'),
-    url(r'^(?P<abbr>[a-z]{2})/(?P<collection_name>\w+)/(?P<_id>\w+)/'
+    url(r'^(?P<abbr>[a-z-]+)/(?P<collection_name>\w+)/(?P<_id>\w+)/'
         'votes/rss/$',
         VotesListFeed(), name='votes_list_rss'),
 
@@ -56,69 +56,69 @@ urlpatterns += patterns('',
 
 # region specific
 urlpatterns += patterns('billy.web.public.views.region',
-    url(r'^(?P<abbr>[a-z]{,3})/search/$', 'search', name='search'),
-    url(r'^(?P<abbr>[a-z]{,3})/search/show_more_legislators/$',
+    url(r'^(?P<abbr>[a-z-]+)/search/$', 'search', name='search'),
+    url(r'^(?P<abbr>[a-z-]+)/search/show_more_legislators/$',
         ShowMoreLegislators.as_view(), name='show_more_legislators'),
-    url(r'^(?P<abbr>[a-z]{2})/$', 'region', name='region'),
+    url(r'^(?P<abbr>[a-z-]+)/$', 'region', name='region'),
     url(r'^region_selection/$', 'region_selection', name='region_selection'),
 )
 
 # events
 urlpatterns += patterns('billy.web.public.views.events',
-    url(r'^(?P<abbr>[a-z]{2})/events/$', EventsList.as_view(),
+    url(r'^(?P<abbr>[a-z-]+)/events/$', EventsList.as_view(),
         name='events'),
-    url(r'^(?P<abbr>[a-z]{2})/events/rss/$', EventsFeed(),
+    url(r'^(?P<abbr>[a-z-]+)/events/rss/$', EventsFeed(),
         name='events_rss'),
-    url(r'^(?P<abbr>[a-z]{2})/events/(?P<event_id>\w+)/', 'event',
+    url(r'^(?P<abbr>[a-z-]+)/events/(?P<event_id>\w+)/', 'event',
         name='event'),
-    url(r'^(?P<abbr>[a-z]{2})/ical/(?P<event_id>\w+)/', 'event_ical',
+    url(r'^(?P<abbr>[a-z-]+)/ical/(?P<event_id>\w+)/', 'event_ical',
         name='event_ical'),
 )
 
 # committees
 urlpatterns += patterns('billy.web.public.views.committees',
-    url(r'^(?P<abbr>[a-z]{2})/committees/$', 'committees', name='committees'),
-    url(r'^(?P<abbr>[a-z]{2})/committees/(?P<committee_id>[A-Z]{3}\d+)/',
+    url(r'^(?P<abbr>[a-z-]+)/committees/$', 'committees', name='committees'),
+    url(r'^(?P<abbr>[a-z-]+)/committees/(?P<committee_id>[A-Z]{3}\d+)/',
         'committee', name='committee'),
 )
 
 # legislators
 urlpatterns += patterns('billy.web.public.views.legislators',
 
-    url(r'^(?P<abbr>[a-z]{2})/legislators/$', 'legislators',
+    url(r'^(?P<abbr>[a-z-]+)/legislators/$', 'legislators',
         name='legislators'),
-    url(r'^(?P<abbr>[a-z]{2})/legislators/(?P<_id>\w+)/(?P<slug>[^/]*)/$',
+    url(r'^(?P<abbr>[a-z-]+)/legislators/(?P<_id>\w+)/(?P<slug>[^/]*)/$',
         'legislator', name='legislator'),
-    url(r'^(?P<abbr>[a-z]{2})/legislators/(?P<_id>\w+)/$',
+    url(r'^(?P<abbr>[a-z-]+)/legislators/(?P<_id>\w+)/$',
         'legislator', name='legislator_noslug'),
 )
 
 # speeches
 urlpatterns += patterns('billy.web.public.views.speeches',
-    url(r'^(?P<abbr>[a-z]{2})/speeches/$', 'speeches',
+    url(r'^(?P<abbr>[a-z-]+)/speeches/$', 'speeches',
         name='speeches'),
-    url(r'^(?P<abbr>[a-z]{2})/speeches/(?P<event_id>\w+)/', 'speeches_by_event',
+    url(r'^(?P<abbr>[a-z-]+)/speeches/(?P<event_id>\w+)/', 'speeches_by_event',
         name='speeches_by_event'),
 )
 
 # bills
 urlpatterns += patterns('billy.web.public.views.bills',
-    url(r'^(?P<abbr>[a-z]{2})/bills/$', BillList.as_view(), name='bills'),
-    url(r'^(?P<abbr>[a-z]{2})/bills/feed/$', BillFeed.as_view(),
+    url(r'^(?P<abbr>[a-z-]+)/bills/$', BillList.as_view(), name='bills'),
+    url(r'^(?P<abbr>[a-z-]+)/bills/feed/$', BillFeed.as_view(),
         name='bills_feed'),
     url(r'^(?P<abbr>all)/bills/$', AllBillList.as_view(), name='bills'),
-    url(r'^(?P<abbr>[a-z]{2})/bills/(?P<session>[^/]+)/(?P<bill_id>[^/]+)/$',
+    url(r'^(?P<abbr>[a-z-]+)/bills/(?P<session>[^/]+)/(?P<bill_id>[^/]+)/$',
         'bill', name='bill'),
-    url(r'^(?P<abbr>[a-z]{2})/(?P<bill_id>[^/]+)/$',
+    url(r'^(?P<abbr>[a-z-]+)/(?P<bill_id>[^/]+)/$',
         'bill_noslug', name='bill_noslug'),
-    url(r'^(?P<abbr>[a-z]{2})/bills/(?P<session>[^/]+)/'
+    url(r'^(?P<abbr>[a-z-]+)/bills/(?P<session>[^/]+)/'
         r'(?P<bill_id>[^/]+)/(?P<key>documents)/$', 'all_documents',
         name='bill_all_documents'),
-    url(r'^(?P<abbr>[a-z]{2})/bills/(?P<session>[^/]+)/'
+    url(r'^(?P<abbr>[a-z-]+)/bills/(?P<session>[^/]+)/'
         r'(?P<bill_id>[^/]+)/documents/(?P<doc_id>\w+)/$', 'document',
         name='document'),
-    url(r'^(?P<abbr>[a-z]{2})/bills/(?P<session>[^/]+)/'
+    url(r'^(?P<abbr>[a-z-]+)/bills/(?P<session>[^/]+)/'
         r'(?P<bill_id>[^/]+)/(?P<key>versions)/$', 'all_versions',
         name='bill_all_versions'),
-    url(r'^(?P<abbr>[a-z]{2})/votes/(?P<vote_id>\w+)/$', 'vote', name='vote'),
+    url(r'^(?P<abbr>[a-z-]+)/votes/(?P<vote_id>\w+)/$', 'vote', name='vote'),
 )
