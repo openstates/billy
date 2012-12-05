562a0ba30ffee627a4adf722e60989cb80e835d1
Thom Neale <twneale@gmail.com>
2012-12-05 11:40:13 -0500

    Refactor api_tests in tests

---
+++ billy/web/api/tests/__init__.py
@@ -0,0 +1,12 @@
+'''
+The django test runner looks for unittest.TestCase subclasses
+defined in this file, so import any testcases here.
+'''
+from .metadata import *
+from .bills import *
+from .legislators import *
+from .committees import *
+from .events import *
+from .auth import *
+from .districts import *
+from .boundaries import *
