c1e147ebec19ed20605226879a51c67a15b3c3da
James Turk <james.p.turk@gmail.com>
2013-01-31 14:55:19 -0500

    changes to ES push task

--- billy/tasks/fulltext.py
+++ billy/tasks/fulltext.py
@@ -1,25 +1,52 @@
 import logging
 from celery.task.base import Task
-from billy.core import db, elasticsearch
+import boto.s3
+import scrapelib
+from billy.core import db, elasticsearch, settings, s3bucket
 from billy.utils.fulltext import plaintext
+
+_log = logging.getLogger('billy.tasks.fulltext')
+
+
+def s3_get(doc):
+    k = boto.s3.key.Key(s3bucket)
+    k.key = 'documents/{0}/{1}'.format(doc[settings.LEVEL_FIELD], doc['_id'])
+
+    # try and get the object, if it doesn't exist- pull it down
+    try:
+        return k.get_contents_as_string()
+    except:
+        doc = db.tracked_versions.find_one(id)
+        if not doc:
+            return None
+        data = scrapelib.urlopen(doc['url'].replace(' ', '%20'))
+        content_type = data.response.headers['content-type']
+        headers = {'x-amz-acl': 'public-read', 'Content-Type': content_type}
+        k.set_contents_from_string(data.bytes, headers=headers)
+        _log.debug('pushed %s to s3 as %s', doc['url'], id)
+        return data.bytes
 
 
 class ElasticSearchPush(Task):
     # results go into ES
     ignore_result = True
-    _log = logging.getLogger('billy.tasks.ElasticSearchPush')
 
     def run(self, doc_id):
         doc = db.tracked_versions.find_one(doc_id)
 
         try:
-            text = plaintext(doc_id)
+            # get bytes, using s3 as a cache
+            doc_bytes = s3_get(doc)
 
+            # extract cleaned text from bytes
+            text = plaintext(doc, doc_bytes)
+
+            # push to elasticsearch
             elasticsearch.index(dict(doc, text=text), 'bills', 'version',
                                 id=doc_id)
-            db.tracked_versions.update({'_id': doc_id},
-                                       {'$set': {'_elasticsearch': True}},
-                                       safe=True)
+
+            doc['_elasticsearch'] = True
+            db.tracked_versions.save(doc, safe=True)
             self._log.info('pushed %s to ElasticSearch', doc_id)
 
         except Exception:
--- billy/utils/fulltext.py
+++ billy/utils/fulltext.py
@@ -1,18 +1,12 @@
 import os
 import re
 import string
-import logging
 import tempfile
 import importlib
 import subprocess
 
 from billy.scrape.utils import convert_pdf
-from billy.core import settings, db, s3bucket
-
-import boto.s3
-import scrapelib
-
-log = logging.getLogger('billy.utils.fulltext')
+from billy.core import settings
 
 
 def pdfdata_to_text(data):
@@ -58,29 +52,14 @@ def id_to_url(id):
     return 'http://{0}/{1}/{2}'.format(settings.AWS_BUCKET, abbr, id)
 
 
-def s3_get(id):
-    k = boto.s3.key.Key(s3bucket)
-    k.key = 'documents/{0}/{1}'.format(id[0:2].lower(), id)
-
-    # try and get the object, if it doesn't exist- pull it down
-    try:
-        return k.get_contents_as_string()
-    except:
-        doc = db.tracked_versions.find_one(id)
-        if not doc:
-            return None
-        data = scrapelib.urlopen(doc['url'].replace(' ', '%20'))
-        content_type = data.response.headers['content-type']
-        headers = {'x-amz-acl': 'public-read', 'Content-Type': content_type}
-        k.set_contents_from_string(data.bytes, headers=headers)
-        log.debug('pushed %s to s3 as %s', doc['url'], id)
-        return data.bytes
-
-
 PUNCTUATION = re.compile('[%s]' % re.escape(string.punctuation))
 
 
-def _clean_text(text):
+def plaintext(doc, doc_bytes):
+    # use module to pull text out of the bytes
+    module = importlib.import_module(doc[settings.LEVEL_FIELD])
+    text = module.extract_text(doc, doc_bytes)
+
     if isinstance(text, unicode):
         text = text.encode('ascii', 'ignore')
     else:
@@ -89,11 +68,3 @@ def _clean_text(text):
     text = PUNCTUATION.sub(' ', text)   # strip punctuation
     text = re.sub('\s+', ' ', text)     # collapse spaces
     return text
-
-
-def plaintext(id):
-    abbr = id[0:2].lower()
-    doc = db.tracked_versions.find_one(id)
-    module = importlib.import_module(abbr)
-    text = module.extract_text(doc, s3_get(id))
-    return _clean_text(text)
