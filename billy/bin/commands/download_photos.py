7f85df9464d4dec6a8d052def11158e669dfa3c6
James Turk <james.p.turk@gmail.com>
2013-01-31 15:02:19 -0500

    timeout=false for cursor

--- billy/bin/commands/download_photos.py
+++ billy/bin/commands/download_photos.py
@@ -61,7 +61,8 @@ class DownloadPhotos(BaseCommand):
                     os.makedirs(d)
 
             for leg in db.legislators.find({settings.LEVEL_FIELD: abbr,
-                                            'photo_url': {'$exists': True}}):
+                                            'photo_url': {'$exists': True}},
+                                           timeout=False):
 
                 fname = os.path.join(orig_dir, '{0}.jpg'.format(leg['_id']))
 
