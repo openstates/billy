a4809aa9ca00b17ca2eedd172810ca6b25daeb23
twneale <twneale@gmail.com>
2013-12-18 17:08:30 -0500

    Added by BFF, the CachedAttr decorator

--- billy/utils/__init__.py
+++ billy/utils/__init__.py
@@ -154,3 +154,27 @@ def cd(path):
         yield
     finally:
         os.chdir(old_dir)
+
+
+class CachedAttr(object):
+    '''Computes attribute value and caches it in instance.
+
+    Example:
+        class MyClass(object):
+            def myMethod(self):
+                # ...
+            myMethod = CachedAttribute(myMethod)
+    Use "del inst.myMethod" to clear cache.
+
+    Source: http://code.activestate.com/recipes/276643-caching-and-aliasing-with-descriptors/'''
+
+    def __init__(self, method, name=None):
+        self.method = method
+        self.name = name or method.__name__
+
+    def __get__(self, inst, cls):
+        if inst is None:
+            return self
+        result = self.method(inst)
+        setattr(inst, self.name, result)
+        return result
