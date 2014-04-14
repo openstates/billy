05a181e6d0f0a7979da75faa437ab856b78047e0
Paul Tagliamonte <tag@pault.ag>
2014-04-14 13:01:15 -0400

    Add API key code.

--- openstates/people.py
+++ openstates/people.py
@@ -1,7 +1,27 @@
 from pupa.scrape import Scraper, Legislator
+import os.path
+import os
 
 
 class OpenstatesPersonScraper(Scraper):
+
+    def __init__(self, *args, **kwargs):
+        super(OpenstatesPersonScraper, self).__init__(*args, **kwargs)
+
+        self.apikey = os.getenv('SUNLIGHT_API_KEY')
+        if self.apikey is None:
+            fp = os.path.expanduser("~/.sunlight.key")
+            try:
+                with open(fp, 'r') as fd:
+                    self.apikey = fd.read().strip()
+            except IOError:
+                pass
+
+        if self.apikey is None:
+            raise ValueError(
+                "No API key found in `SUNLIGHT_API_KEY` envvar or "
+                "~/.sunlight.key"
+            )
 
     def api(self, method):
         url = 'http://openstates.org/api/v1/' + method + '&apikey=' + self.apikey
