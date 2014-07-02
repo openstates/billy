6ab20278ac8e60b0bd177972f7cef3a11397dfac
James Turk <james.p.turk@gmail.com>
2014-07-02 18:06:52 -0400

    try out a generic make_jurisdiction piece

--- ak/__init__.py
+++ ak/__init__.py
@@ -1,37 +1,9 @@
-from pupa.scrape import Jurisdiction, Organization, Post
-from openstates.people import OpenstatesPersonScraper
-from openstates.bills import OpenstatesBillScraper
+from openstates.jurisdiction import make_jurisdiction
 
+Alaska = make_jurisdiction('ak')
 
-class PersonScraper(OpenstatesPersonScraper):
-    state = 'ak'
-
-class BillScraper(OpenstatesBillScraper):
-    state = 'ak'
-
-house = Organization('Alaska State House', classification='legislature', chamber='lower')
-senate = Organization('Alaska State Senate', classification='legislature', chamber='upper')
-
-
-class Alaska(Jurisdiction):
-    division_id = "ocd-division/country:us/state:ak"
-    classification = "government"
-    name = "Alaska State Legislature"
-    url = "http://w3.legis.state.ak.us/"
-    legislative_sessions = [
-        {"name": '28'},
-        {"name": '27'},
-        {"name": '26'},
-    ]
-
-    scrapers = {
-        "people": PersonScraper,
-        "bills": BillScraper,
-    }
-    parties = [{'name': 'Republican'}, {'name': 'Democratic'}]
-
-    organizations = [house, senate]
-    posts = ([Post(label=str(n), role='Representative', organization_id=house._id)
-              for n in range(1, 41)] +
-             [Post(label=chr(n), role='Senator', organization_id=senate._id)
-              for n in range(65, 85)])
+Alaska.posts = (
+    [Post(label=str(n), role='Representative', chamber='lower') for n in range(1, 41)] +
+    [Post(label=chr(n), role='Senator', chamber='upper') for n in range(65, 85)]
+)
+Alaska.url = 'http://alaska.gov'
---
+++ openstates/jurisdiction.py
@@ -0,0 +1,74 @@
+from pupa.scrape import Jurisdiction, Organization
+from openstates.base import OpenstatesBaseScraper
+from openstates.people import OpenstatesPersonScraper
+from openstates.bills import OpenstatesBillScraper
+
+def chamber_name(state, chamber):
+    if state in ('ne', 'dc', 'pr'):
+        raise ValueError(state)
+
+    if chamber == 'lower':
+        if state in ('ca', 'ny', 'wi'):
+            return 'State Assembly'
+        elif state in ('md', 'va', 'wv'):
+            return 'House of Delegates'
+        elif state == 'nv':
+            return 'Assembly'
+        elif state == 'nj':
+            return 'General Assembly'
+        else:
+            return 'House of Representatives'   # 41 of these
+    elif chamber == 'upper':
+        if state in ('ca', 'ga', 'la', 'ms', 'ny', 'or', 'pa', 'wa', 'wi'):
+            return 'State Senate'
+        else:
+            return 'Senate'
+
+
+def make_jurisdiction(a_state):
+
+    osbs = OpenstatesBaseScraper(None, None)
+    metadata = osbs.api('metadata/{}?'.format(a_state))
+
+    # timezone
+    # legislature_name
+    # legislature_url
+    # chambers.title
+    # terms?
+    # sessions
+
+    # make orgs
+    orgs = []
+    for otype in ('upper', 'lower'):
+        if otype in metadata['chambers']:
+            org = Organization(metadata['name'] + ' ' + chamber_name(a_state, otype),
+                               classification='legislature', chamber=otype)
+            orgs.append(org)
+
+    leg_sessions = []
+    for td in metadata['terms']:
+        for s in td['sessions']:
+            session = {'identifier': s,
+                       'name': metadata['session_details'][s]['display_name'],
+                       'start_date': metadata['session_details'][s].get('start_date', ''),
+                       'end_date': metadata['session_details'][s].get('end_date', ''),
+                      }
+            leg_sessions.append(session)
+
+
+    # make scrapers
+    class PersonScraper(OpenstatesPersonScraper):
+        state = a_state
+    class BillScraper(OpenstatesBillScraper):
+        state = a_state
+
+    class StateJuris(Jurisdiction):
+        division_id = 'ocd-division/country:us/state:' + a_state
+        classification = 'government'
+        name = metadata['name']
+
+        scrapers = {'people': PersonScraper, 'bills': BillScraper}
+        parties = [{'name': 'Republican'}, {'name': 'Democratic'}]
+        sessions = leg_sessions
+
+    return StateJuris
