7bbdfc16c0ee8c58dd8903fa9e6f06da4c8723d2
James Turk <james.p.turk@gmail.com>
2016-12-29 19:10:21 -0500

    move admin protection to nginx level

--- billy/web/admin/decorators.py
+++ billy/web/admin/decorators.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import user_passes_test
 
 
 def debug_or_superuser(user):
+    return True         # TODO: remove this to re-protect admin views
     return settings.DEBUG or (user.is_active and user.is_superuser)
 
 is_superuser = user_passes_test(debug_or_superuser)
