dc6eceaf640108c562063094394dc6d992015d7a
James Turk <james.p.turk@gmail.com>
2017-04-18 23:23:51 -0400

    s/patterns//

--- billy/web/urls.py
+++ billy/web/urls.py
@@ -1,14 +1,12 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+from django.contrib.staticfiles.views import serve
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     (r'^api/', include('billy.web.api.urls')),
     (r'^admin/', include('billy.web.admin.urls')),
     (r'^public/', include('billy.web.public.urls')),
-)
+]
 
 if settings.DEBUG:
-    urlpatterns += patterns('django.contrib.staticfiles.views',
-                            url(r'^static/(?P<path>.*)$', 'serve'),
-)
+    urlpatterns += [url(r'^static/(?P<path>.*)$', serve)],
