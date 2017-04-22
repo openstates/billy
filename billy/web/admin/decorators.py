ca46538aa68dede7ed9b66cccf10ac38904936c2
James Turk <james.p.turk@gmail.com>
2017-04-21 20:53:25 -0400

    restore is_superuser check for /admin/

--- billy/web/admin/decorators.py
+++ billy/web/admin/decorators.py
@@ -1,9 +1,7 @@
-from django.conf import settings
 from django.contrib.auth.decorators import user_passes_test
 
 
 def debug_or_superuser(user):
-    return True         # TODO: remove this to re-protect admin views
-    return settings.DEBUG or (user.is_active and user.is_superuser)
+    return (user.is_active and user.is_superuser)
 
-is_superuser = user_passes_test(debug_or_superuser)
+is_superuser = user_passes_test(debug_or_superuser, login_url='/djadmin/login/')
